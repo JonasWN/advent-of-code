af2e72caaf24f1b802796076e074179fb74bafa5
Jonas Wessman Nielsen <jonaswessmannielsen@Jonas-MacBook-Pro.local>
2022-12-04 12:47:55 +0100

    Day 4

---
+++ 2022/04/code_02.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
+    const solution = data.trim()
+        .split('\n')
+        .reduce((counter, line) => {
+            const [first, second] = line.split(',')
+            const [a, b] = first.split('-').map((n) => +n)
+            const [c, d] = second.split('-').map((j) => +j)
+            const rangeOverlaps = b >= c && a <= d
+
+            if(rangeOverlaps) {
+                return counter + 1
+            }
+
+            return counter
+        }, 0)
+
+    console.log(solution)
+})
